5019d691763e025631c5ffa35e1bedb97acc8326
KlausMikhaelson <satyamsingh5076@gmail.com>
2024-08-10 23:13:11 -0600

    fixing imports

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import "openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/openzeppelin-contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/openzeppelin-contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721, Ownable {
     uint256 public nextTokenId;
